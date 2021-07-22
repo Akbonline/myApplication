96dbd31a06d878c9773ee5bd0f42306a671e20d0
Akshat Bajpai <akshatbajpai.biz@gmail.com>
2021-07-22 08:56:53 -0700

    Updated CMakeList and gradle.build

--- app/src/main/cpp/cmake_install.cmake
+++ app/src/main/cpp/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/akshatbajpai/Desktop/Work/MyApplication/app/src/main/cpp/hello_world
+# Install script for directory: /Users/akshatbajpai/Desktop/Work/myApplication copy/app/src/main/cpp/NFIQ_2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,39 @@ endif()
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xnfiq2_clix" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/nfiq2/bin" TYPE DIRECTORY PERMISSIONS OWNER_READ OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE FILES "/Users/akshatbajpai/Desktop/Work/MyApplication/app/src/main/cpp/install_staging/nfiq2/bin/")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xnfiq2_clix" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/nfiq2/share" TYPE DIRECTORY FILES "/Users/akshatbajpai/Desktop/Work/MyApplication/app/src/main/cpp/install_staging/nfiq2/share/")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xdevelopmentx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/nfiq2/lib" TYPE DIRECTORY FILES "/Users/akshatbajpai/Desktop/Work/MyApplication/app/src/main/cpp/install_staging/nfiq2/lib/")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xdevelopmentx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/nfiq2/include" TYPE DIRECTORY FILES "/Users/akshatbajpai/Desktop/Work/MyApplication/app/src/main/cpp/install_staging/nfiq2/include/")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xnfiq2_clix" OR NOT CMAKE_INSTALL_COMPONENT)
+  execute_process(COMMAND /Users/akshatbajpai/Library/Android/sdk/cmake/3.10.2.4988404/bin/cmake -E make_directory /Users/akshatbajpai/Desktop/Work/MyApplication/app/src/main/cpp/install_staging/nfiq2/../bin)
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xnfiq2_clix" OR NOT CMAKE_INSTALL_COMPONENT)
+  execute_process(COMMAND ln -s -f "../nfiq2/bin/nfiq2" "nfiq2" WORKING_DIRECTORY "/Users/akshatbajpai/Desktop/Work/MyApplication/app/src/main/cpp/install_staging/nfiq2/../bin")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xnfiq2_clix" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE FILE FILES "/Users/akshatbajpai/Desktop/Work/MyApplication/app/src/main/cpp/install_staging/nfiq2/../bin/nfiq2")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/Users/akshatbajpai/Desktop/Work/MyApplication/app/src/main/cpp/fingerjetfxose/FingerJetFXOSE/libFRFXLL/src/cmake_install.cmake")
+
 endif()
 
